64b132bdbfeb50daebbd42c9aceb01dc6db3897d
Rinor <76968605+Rinqa@users.noreply.github.com>
2021-05-01 15:46:13 +0200

    BackEndCoresEnable

--- Back-End/Holiday/Holiday/Startup.cs
+++ Back-End/Holiday/Holiday/Startup.cs
@@ -9,7 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.Extensions.Options;
+using Newtonsoft.Json.Serialization;
+using System.IO;
+using Microsoft.Extensions.FileProviders;
 
 namespace Holiday
 {
@@ -29,15 +31,19 @@ namespace Holiday
             {
                 c.AddPolicy("AllowOrigin",options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             });
-            services.AddControllersWithViews(
-                .AddNewtonsoftJson(options =>options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
-                )
+            services.AddControllersWithViews()
+                 .AddNewtonsoftJson(options =>
+                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+                 .Json.ReferenceLoopHandling.Ignore)
+                 .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+                 = new DefaultContractResolver());
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
