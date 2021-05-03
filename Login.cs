0a9fa66f91670defb0eeb0a9226e6af4cd145275
albinsylejmani <56934932+albinsylejmani@users.noreply.github.com>
2021-05-03 21:42:45 +0200

    Add files via upload

---
+++ Login.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Login.Models
+{
+    public class Login
+    {
+        public string Email { get; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Email is incorrect!, Please try again.")]
+        public string Password { get; }
+        [System.ComponentModel.DataAnnotations.Required(ErrorMessage = "Password is incorrect!, Please try again.")]
+
+    }
+}
