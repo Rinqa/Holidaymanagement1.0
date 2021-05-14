f00abb7090822216ccf18b57fa5b9902d4e7957c
albinsylejmani <56934932+albinsylejmani@users.noreply.github.com>
2021-05-14 13:20:48 +0200

    Update PozitaController.cs

--- Back-End/Eleaving/Eleaving/Controllers/PozitaController.cs
+++ Back-End/Eleaving/Eleaving/Controllers/PozitaController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
+using Eleaving.Models;
 
 namespace Eleaving.Controllers
 {
@@ -11,5 +15,118 @@ namespace Eleaving.Controllers
     [ApiController]
     public class PozitaController : ControllerBase
     {
+        private readonly IConfiguration _configuration;
+
+        public PozitaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+
+        public JsonResult Get()
+        {
+            string query = @"
+                    select Id, Pozita";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader);
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+
+        [HttpPost]
+
+        public JsonResult Post(Pozita poz)
+        {
+            string query = @"
+                    insert into Pozita values
+                    ('" + poz.Pozita + @"')
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+
+            [HttpPut]
+
+            public JsonResult Put(Pozita poz)
+            {
+                string query = @"
+                    update Pozita set 
+                    Departamenti = '" + poz.Pozita + @"'
+                    where Id  = " + poz.Id + @" 
+                    ";
+                DataTable table = new DataTable();
+                string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+                SqlDataReader myReader;
+                using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+                {
+                    myCon.Open();
+                    using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                    {
+                        myReader = myCommand.ExecuteReader();
+                        table.Load(myReader); ;
+
+                        myReader.Close();
+                        myCon.Close();
+                    }
+                }
+
+            }
+            return new JsonResult("Updated Successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from Pozita
+                    where Id = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
     }
 }
+
