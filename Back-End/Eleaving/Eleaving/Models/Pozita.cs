71af44e2949efc906690376aceb444ee99e2b7f7
Rinor <76968605+Rinqa@users.noreply.github.com>
2021-05-14 12:51:43 +0200

    Create Pozita.cs

---
+++ Back-End/Eleaving/Eleaving/Models/Pozita.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eleaving.Models
+{
+    public class Pozita
+    {
+        public int Id { get; set; }
+        public string poz { get; set; }
+    }
+}
