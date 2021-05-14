d8147ab726b0c10a9b4471a906c2c85bd76bccb1
Rinor <76968605+Rinqa@users.noreply.github.com>
2021-05-14 13:19:41 +0200

    KompCont, KatPushCon, PushimetController

---
+++ Back-End/Eleaving/Eleaving/Controllers/KompaniaController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Eleaving.Models;
+
+namespace Eleaving.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    public class KompaniaController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+        public KompaniaController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from Kompania";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult(table);
+        }
+        [HttpPost]
+        public JsonResult Post(Kompania komp)
+        {
+            string query = @"
+                    insert into kompania values 
+                    ('" + komp.Emri + @"')
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Added Successfully");
+        }
+        [HttpPut]
+        public JsonResult Put(Kompania komp)
+        {
+            string query = @"
+                    update Kompania set 
+                    Emri = '" + komp.Emri + @"'
+                    where Id  = " + komp.Id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Updated Successfully");
+        }
+        [HttpDelete("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    delete from Kompania
+                    where Id = " + id + @" 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+
+}
---
+++ Back-End/Eleaving/Eleaving/Models/Pushimet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eleaving.Models
+{
+    public class Pushimet
+    {
+        public int Id { get; set; }
+        public string Pushimi { get; set; }
+        public int Kategoria { get; set; }
+
+    }
+}
