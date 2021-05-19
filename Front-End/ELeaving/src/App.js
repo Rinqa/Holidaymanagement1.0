47333b46daaea23e439184604b3273da45bfe98d
Rinor <76968605+Rinqa@users.noreply.github.com>
2021-05-19 13:49:43 +0200

    AdminPages

--- Front-End/ELeaving/src/App.js
+++ Front-End/ELeaving/src/App.js
@@ -1,39 +1,26 @@
-import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
-import './scss/style.scss';
-
-const loading = (
-  <div className="pt-3 text-center">
-    <div className="sk-spinner sk-spinner-pulse"></div>
-  </div>
-)
-
-// Containers
-const TheLayout = React.lazy(() => import('./containers/TheLayout'));
-
-// Pages
-const Login = React.lazy(() => import('./views/pages/login/Login'));
-const Register = React.lazy(() => import('./views/pages/register/Register'));
-const Page404 = React.lazy(() => import('./views/pages/page404/Page404'));
-const Page500 = React.lazy(() => import('./views/pages/page500/Page500'));
-
-class App extends Component {
-
-  render() {
-    return (
-      <HashRouter>
-          <React.Suspense fallback={loading}>
-            <Switch>
-              <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
-              <Route exact path="/register" name="Register Page" render={props => <Register {...props}/>} />
-              <Route exact path="/404" name="Page 404" render={props => <Page404 {...props}/>} />
-              <Route exact path="/500" name="Page 500" render={props => <Page500 {...props}/>} />
-              <Route path="/" name="Home" render={props => <TheLayout {...props}/>} />
-            </Switch>
-          </React.Suspense>
-      </HashRouter>
-    );
-  }
+import React from 'react'
+import {Admin, Resource} from 'react-admin'
+import restProvider from 'ra-data-simple-rest'
+import Users from './components/Users'
+import Pushimet from './components/Pushimet'
+import Departamenti from './components/Departamenti'
+import Aplikimet from './components/Aplikimet'
+function App() {
+  return (
+    <Admin dataProvider={restProvider('http://localhost:3000/admin')}>
+      <Resource name="users" list={Users}></Resource>
+      <Resource name="Pushimet" list={Pushimet}></Resource>
+      <Resource name="Departamentet" list={Departamenti}></Resource>
+      <Resource name="Aplikimet" list={Aplikimet}></Resource>
+      <Resource name="Festat Zyrtare" list={Pushimet}></Resource>
+      <Resource name="Kompania" list={Pushimet}></Resource>
+      <Resource name="Pozitat" list={Pushimet}></Resource>
+      <Resource name="Roli" list={Pushimet}></Resource>
+      <Resource name="Pushimet e marrura" list={Pushimet}></Resource>
+      <Resource name="Statusi Puntorit" list={Pushimet}></Resource>
+      <Resource name="Kategorit e Pushimeve" list={Pushimet}></Resource>
+    </Admin>
+  );
 }
 
 export default App;
--- Front-End/ELeaving/src/index.js
+++ Front-End/ELeaving/src/index.js
@@ -1,27 +1,17 @@
-import 'react-app-polyfill/ie11'; // For IE 11 support
-import 'react-app-polyfill/stable';
-import 'core-js';
-import './polyfill'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-import { icons } from './assets/icons'
-
-import { Provider } from 'react-redux'
-import store from './store'
-
-React.icons = icons
+import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App/>
-  </Provider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
