b4c7985b5f5491f5c0bb48c96f364b6d6c8933ff
Rinor <76968605+Rinqa@users.noreply.github.com>
2021-05-22 22:22:14 +0200

    db edit

--- DB.sql
+++ DB.sql
@@ -67,7 +67,7 @@ create table FestatZyrtare(
 
 create table Aplikimet(
 	Id int primary key identity(1,1),
-	Users int references Users(Id),
+	IdUser int,
 	Pushimi varchar(255),
 	DataFillimit date,
 	DataMbarimit date,
