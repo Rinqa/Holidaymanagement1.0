4107cac4c6cb0dbee37054c3e16b31fbdcd0ee08
albinsylejmani <56934932+albinsylejmani@users.noreply.github.com>
2021-06-01 10:56:40 +0200

    ?

---
+++ Back-End/Eleaving/Eleaving/Controllers/GetPushimetController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Eleaving.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetPushimetController : ControllerBase
+    {
+        private IConfiguration _configuration;
+
+        public GetPushimetController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet("{id}")]
+        public JsonResult Delete(int id)
+        {
+            string query = @"
+                    select * from PushimetMarrura
+                    where Id = " + id + @" order by Viti 
+                    ";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("ElavingApp");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDataSource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = myCommand.ExecuteReader();
+                    table.Load(myReader); ;
+
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+
+            return new JsonResult("Deleted Successfully");
+        }
+    }
+}
